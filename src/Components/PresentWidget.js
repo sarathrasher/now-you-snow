3577c90ef2ec0c8bfa3be5b6e9fd7c64bfbdd936
Sara Muntean <munteansr@gmail.com>
2018-10-15 12:10:35 -0400

    Removed unnecessary onClick function for widget

--- src/Components/PresentWidget.js
+++ src/Components/PresentWidget.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
 let PresentWidget = (props) =>
-  <div 
-  className='weather-widget'
-  onClick={() =>
-    props.handleClick(props.index)
-  }
-  >
+  <div className='weather-widget'>
     <p className={'date'}>{props.weatherResults.item.forecast[0].date}</p>
-      <div className='expanded-details'>
-        <p className='high'>High: {props.weatherResults.item.forecast[0].high}</p>
-        <p className='high'>Low: {props.weatherResults.item.forecast[0].low}</p>
-        <p className='description'>{props.weatherResults.item.forecast[0].text}</p>
-        <p className='wind'>Wind chill: {props.weatherResults.wind.chill}F</p>
-      </div>
+    <div className='expanded-details'>
+      <p className='high'>High: {props.weatherResults.item.forecast[0].high}</p>
+      <p className='high'>Low: {props.weatherResults.item.forecast[0].low}</p>
+      <p className='description'>{props.weatherResults.item.forecast[0].text}</p>
+      <p className='wind'>Wind chill: {props.weatherResults.wind.chill}F</p>
+    </div>
   </div>
 
 export default PresentWidget;
