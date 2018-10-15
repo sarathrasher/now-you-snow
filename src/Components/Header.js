bd9c96106907601a4d1665adb7863750b847fb19
Sara Muntean <munteansr@gmail.com>
2018-10-15 14:01:32 -0400

    Initial styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import SmartLocationForm from './LocationForm'
-import SmartLocationListBar from './LocationListBar'
 
 let Header = () =>
-  <div class='header'>
+  <div className='header'>
     <h1 className="header-text">Now You Snow</h1>
-    <SmartLocationForm />
-    <SmartLocationListBar />
   </div>
 
 export default Header;
--- src/Components/LocationScreen.js
+++ src/Components/LocationScreen.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import PresentWidget from './PresentWidget';
 import FutureComponent from './FutureComponent';
+import SmartLocationForm from './LocationForm'
+import SmartLocationListBar from './LocationListBar'
 
 class LocationScreen extends React.Component {
   constructor(props) {
@@ -17,43 +19,48 @@ class LocationScreen extends React.Component {
     return (
       <div className="main">
         <Header />
+        <div className='content'>
+        <SmartLocationForm />
+        <SmartLocationListBar />
         <div className='date-nav-bar'>
           <p 
-            className={this.state.currentPage === 'present' ? 'nav-link bold' : 'nav-link'} 
+            className={this.state.currentPage === 'present' ? 'date-link active' : 'date-link'} 
             onClick={() =>
               this.setState({
                 currentPage: 'present'
               })
             }
-          >Today's Weather</p>
+          >Today</p>
           <p 
-            className={this.state.currentPage !== 'present' ? 'nav-link bold' : 'nav-link'}
+            className={this.state.currentPage !== 'present' ? 'date-link active' : 'date-link'}
             onClick={() =>
               this.setState({
                 currentPage: 'forecast'
               })
             }
           >Forecast</p>
-        </div>
-        {this.state.currentPage === 'present' 
-          ? 
-          <PresentWidget weatherResults={this.props.weatherResults} /> 
-          :
-          <FutureComponent weatherResults={this.props.weatherResults} />
-        }
-        <button 
-          className='button'
-          onClick={(event) => {
-            event.preventDefault();
-            this.props.dispatch({
-              type: 'DELETE_LOCATION',
-              location: this.props.match.params.location
-            })
-            this.props.history.push('/'); 
-            }
-          }   
-          >Delete Location 
+          </div>
+          {this.state.currentPage === 'present' 
+            ? 
+            <PresentWidget weatherResults={this.props.weatherResults} /> 
+            :
+            <FutureComponent weatherResults={this.props.weatherResults} />
+          }
+          <button 
+            className='button'
+            onClick={(event) => {
+              event.preventDefault();
+              this.props.dispatch({
+                type: 'DELETE_LOCATION',
+                location: this.props.match.params.location
+              })
+              this.props.history.push('/'); 
+              }
+            }   
+            >Delete Location 
           </button>
+        </div>
+       
       </div>
     )
   }
