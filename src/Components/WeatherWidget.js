523ee791b1b304977b8fc9c2c3d53008898ff5c9
Sara Muntean <munteansr@gmail.com>
2018-10-15 12:09:40 -0400

    Removed unnecessary onClick function for widget

--- src/Components/WeatherWidget.js
+++ src/Components/WeatherWidget.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
 let WeatherWidget = (props) =>
-  <div 
-    className='weather-widget'
-    onClick={() =>
-      props.handleClick(props.index)
-    }
-    >
-      <p className='date'>{props.day.date}</p>
-        <div className='expanded-details'>
-          <p className='high'>High: {props.day.high}</p>
-          <p className='high'>Low: {props.day.low}</p>
-          <p className='description'>{props.day.text}</p>
-        </div>
+  <div className='weather-widget'>
+    <p className='date'>{props.day.date}</p>
+    <div className='expanded-details'>
+      <p className='high'>High: {props.day.high}</p>
+      <p className='high'>Low: {props.day.low}</p>
+      <p className='description'>{props.day.text}</p>
+    </div>
   </div>
 
 export default WeatherWidget
